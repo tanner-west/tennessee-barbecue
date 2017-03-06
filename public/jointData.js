dfe940df2a531fec5539d546297ec8ef0aaaeff9
Tanner <west.tanner.j@gmail.com>
2017-03-06 18:31:01 -0500

    first commit

---
+++ public/jointData.js
@@ -0,0 +1,331 @@
+var rawJoints = [{"name":"Weber Bar-B-Q","address":"5960 W Andrew Johnson Hwy, Morristown, TN 37814","LatLng":{"lat":36.17922799999999,"lng":-83.392428}},{"name":"Sweet Swine BBQ","address":"136 W Old Andrew Johnson Hwy. Jefferson City, TN 37760","LatLng":{"lat":36.1248613,"lng":-83.49460850000003}},{"name":"Red Pig Bar-B-Q","address":"2201 Ferguson Rd, Johnson City, TN 37604","LatLng":{"lat":36.3430526,"lng":-82.3779968}},{"name":"Ridgewood Barbecue","address":"900 Elizabethton Hwy, Bluff City, TN 37618","LatLng":{"lat":36.4339344,"lng":-82.26573250000001}},{"name":"Stan's Bar-B-Q","address":"2620 E Andrew Johnson Hwy, Greeneville, TN 37745","LatLng":{"lat":36.17821199999999,"lng":-82.7768835}},{"name":"Smokin Pig BBQ","address":"708 E Church St, Greeneville, TN 37745","LatLng":{"lat":36.1609679,"lng":-82.81915700000002}},{"name":"Top Choice BBQ","address":"919 W Main St, Greeneville, TN 37743","LatLng":{"lat":36.1559116,"lng":-82.8456357}},{"name":"Gigglin Pig","address":"642 W Hwy 25 70, Cocke County, TN 37821","LatLng":{"lat":35.973694,"lng":-83.23689000000002}},{"name":"Pig & Chick","address":"Old Hwy Rd, Rogersville, TN 37857","LatLng":{"lat":36.4570392,"lng":-82.94593800000001}},{"name":"Sweet P's Barbeque & Soul House","address":"3725 Maryville Pike, Knoxville, TN 37920","LatLng":{"lat":35.8737087,"lng":-83.9431113}},{"name":"Sweet P's Barbecue and Downtown Dive","address":"37902, 410 W Jackson Ave, Knoxville, TN 37902","LatLng":{"lat":35.96787,"lng":-83.92167599999999}},
+{
+		"name": "Sticky Fingers",
+		"address": "420 Broad St, Chattanooga, TN 37402",
+		"website": "http://www.stickyfingers.com/",
+		"telephone": "(423) 265-7427",
+		"LatLng": {
+			"lat": 35.0514028,
+			"lng": -85.31029890000002
+		}
+	},
+	{
+		"name": "Hickory Pit Bar-B-Que",
+		"address": "5611 Ringgold Rd B, Chattanooga, TN 37412",
+		"website": "http://hickorypitbarbque.com/",
+		"telephone": " (423) 894-1217",
+		"LatLng": {
+			"lat": 34.9923649,
+			"lng": -85.22562169999998
+		}
+	},
+	{
+		"name": "Sugar's Ribs",
+		"address": "2450 15th Avenue, Chattanooga, TN 37494",
+		"website": "https://www.sugarsribs.com/",
+		"telephone": "(423) 826-1199",
+		"LatLng": {
+			"lat": 35.0134566,
+			"lng": -85.27201029999998
+		}
+	},
+	{
+		"name": "Porker's BBQ",
+		"address": "1251 Market St, Chattanooga, TN 37402",
+		"website": "https://www.sugarsribs.com/",
+		"telephone": "(423) 267-2726",
+		"LatLng": {
+			"lat": 35.0404258,
+			"lng": -85.30828229999997
+		}
+	},
+	{
+		"name": "Chatt Smoke House",
+		"address": "416 E M L King Blvd, Chattanooga, TN 37403",
+		"website": "https://www.dinnerdeliveredonline.org/r/498/restaurants/delivery/Barbeque/Chatt-Smokehouse-Chattanooga",
+		"telephone": "(423) 468-4978",
+		"LatLng": {
+			"lat": 35.043991,
+			"lng": -85.3035304
+		}
+	},
+	{
+		"name": "Old Plantation Barbecue",
+		"address": "1515 Dodson Ave, Chattanooga, TN 37406",
+		"website": "",
+		"telephone": "(423) 624-8105",
+		"LatLng": {
+			"lat": 35.0541188,
+			"lng": -85.26032370000001
+		}
+	},
+	{
+		"name": "HILLBILLY WILLYS B-B-Q",
+		"address": "115 Browns Ferry Rd, Chattanooga, TN 37419",
+		"website": "http://www.hillbillywillys.com/",
+		"telephone": "(423) 821-2272",
+		"LatLng": {
+			"lat": 35.0218659,
+			"lng": -85.36627679999998
+		}
+	},
+	{
+		"name": "Choo Choo Barbecue",
+		"address": "3951 Ringgold Rd, Chattanooga, TN 37412",
+		"website": "http://www.choochoobbqeastridge.com/",
+		"telephone": "(423) 629-1313",
+		"LatLng": {
+			"lat": 34.9979418,
+			"lng": -85.25092230000001
+		}
+	},
+	{
+		"name": "Choo Choo Barbque",
+		"address": "5936 Quintus Loop, Chattanooga, TN 37421",
+		"website": "",
+		"telephone": "(423) 894-7520",
+		"LatLng": {
+			"lat": 35.0534127,
+			"lng": -85.1986296
+		}
+	},
+	{
+		"name": "Rib & Loin",
+		"address": "5946 E Brainerd Rd, Chattanooga, TN 37421",
+		"website": "http://ribandloin.com/",
+		"telephone": "(423) 499-6465",
+		"LatLng": {
+			"lat": 35.0173396,
+			"lng": -85.20540979999998
+		}
+	},
+	{
+		"name": "Purple Daisy Picnic Cafe",
+		"address": "4001 St Elmo Ave, Chattanooga, TN 37409",
+		"website": "http://www.thepurpledaisypicniccafe.com/",
+		"telephone": "(423) 822-6477",
+		"LatLng": {
+			"lat": 35.0083552,
+			"lng": -85.32865149999998
+		}
+	},
+	{
+		"name": "Chubby's Bar-B-Q",
+		"address": "3801 Rossville Blvd, Chattanooga, TN 37407",
+		"website": "http://www.rossvillerestaurant.com/",
+		"telephone": "(423) 867-4422",
+		"LatLng": {
+			"lat": 34.9999178,
+			"lng": -85.29214769999999
+		}
+	},
+	{
+		"name": "Nooga Q Smokehouse Grill",
+		"address": "301 Signal Mountain Rd, Chattanooga, TN 37405",
+		"website": "http://www.nooga-q.com/",
+		"telephone": "(423) 752-1935",
+		"LatLng": {
+			"lat": 35.0861492,
+			"lng": -85.32651959999998
+		}
+	},
+		{
+		"name": "Jack's Bar-B-Que",
+		"address": "416 Broadway, Nashville, TN 37203",
+		"website": "http://jacksbarbque.com/",
+		"telephone": "(615) 254-5715",
+		"LatLng": {
+			"lat": 36.1609383,
+			"lng": -86.77794899999998
+		}
+	},
+	{
+		"name": "Edley's Bar-B-Que",
+		"address": "2706 12th Ave S, Nashville, TN 37204",
+		"website": "http://edleysbbq.com/",
+		"telephone": "(615) 953-2951",
+		"LatLng": {
+			"lat": 36.1229306,
+			"lng": -86.78973500000001
+		}
+	},
+	{
+		"name": "Peg Leg Porker BBQ",
+		"address": "903 Gleaves St, Nashville, TN 37203",
+		"website": "http://peglegporker.com/",
+		"telephone": "(615) 829-6023",
+		"LatLng": {
+			"lat": 36.1516909,
+			"lng": -86.78115129999998
+		}
+	},
+	{
+		"name": "Martin's Bar-B-Que Joint",
+		"address": "3108 Belmont Blvd, Nashville, TN 37212",
+		"website": "http://www.martinsbbqjoint.com/",
+		"telephone": "(615) 200-1181",
+		"LatLng": {
+			"lat": 36.1180327,
+			"lng": -86.7964427
+		}
+	},
+	{
+		"name": "Hog Heaven",
+		"address": "115 27th Ave N, Nashville, TN 37203",
+		"website": "http://www.hogheavenbbq.com/",
+		"telephone": "(615) 329-1234",
+		"LatLng": {
+			"lat": 36.1463468,
+			"lng": -86.81228909999999
+		}
+	},
+	{
+		"name": "Slow & Low BBQ Bistro",
+		"address": "333 54th Ave N, Nashville, TN 37209",
+		"website": "http://www.slowandlowbbqbistro.com/",
+		"telephone": "(615) 385-7800",
+		"LatLng": {
+			"lat": 36.1506907,
+			"lng": -86.85383709999996
+		}
+	},
+	{
+		"name": "B&C Melrose Barbeque",
+		"address": "2617 Franklin Pike, Nashville, TN 37204",
+		"website": "http://baconandcaviar.com/",
+		"telephone": "(615) 457-3473",
+		"LatLng": {
+			"lat": 36.1193237,
+			"lng": -86.7772023
+		}
+	},
+	{
+		"name": "Drifters Tennessee Barbeque (BBQ) Joint, East Nashville",
+		"address": "1008 B Woodland Street, Nashville, TN 37206",
+		"website": "http://www.driftersnashville.com/",
+		"telephone": "(615) 262-2776",
+		"LatLng": {
+			"lat": 36.1769647,
+			"lng": -86.75082850000001
+		}
+	},
+	{
+		"name": "East Nashville BBQ Co.",
+		"address": "829 Lischey Ave, Nashville, TN 37207",
+		"website": "https://www.facebook.com/EastNashvilleBBQCompany/",
+		"telephone": "(615) 645-9613",
+		"LatLng": {
+			"lat": 36.186276,
+			"lng": -86.76416560000001
+		}
+	},
+	{
+		"name": "Bob's Barbecue",
+		"address": "517 Cason Ln a, Murfreesboro, TN 37128",
+		"website": "",
+		"telephone": "(615) 396-0519",
+		"LatLng": {
+			"lat": 35.8416269,
+			"lng": -86.44004889999997
+		}
+	},
+	{
+		"name": "Rick's BBQ",
+		"address": "3364 Memorial Blvd, Murfreesboro, TN 37129",
+		"website": "http://www.ricksbbq.com/#/home",
+		"telephone": "(615) 907-1840",
+		"LatLng": {
+			"lat": 35.9093868,
+			"lng": -86.38340599999998
+		}
+	},
+	{
+		"name": "Slick Pig BBQ",
+		"address": "1920 E Main St, Murfreesboro, TN 37130",
+		"website": "http://www.slickpigbbq.com/",
+		"telephone": "(615) 890-3583",
+		"LatLng": {
+			"lat": 35.8408074,
+			"lng": -86.3596637
+		}
+	},
+	{
+		"name": "im 'N Nick's Bar-B-Q",
+		"address": "436 N Thompson Ln, Murfreesboro, TN 37129",
+		"website": "http://www.jimnnicks.com/",
+		"telephone": "(615) 893-1001",
+		"LatLng": {
+			"lat": 35.8543993,
+			"lng": -86.42979129999998
+		}
+	},
+	{
+		"name": "Woody's Bar-B-Q",
+		"address": "352 W Northfield Blvd F, Murfreesboro, TN 37129",
+		"website": "http://www.woodys.com/",
+		"telephone": "(615) 900-1136",
+		"LatLng": {
+			"lat": 35.8722343,
+			"lng": -86.38971140000001
+		}
+	},
+	{
+		"name": "Hickory Pit",
+		"address": "555 Main St, Savannah, TN 38372",
+		"website": "",
+		"telephone": "(731) 925-2268",
+		"LatLng": {
+			"lat": 35.2249357,
+			"lng": -88.25216039999998
+		}
+	}
+
+
+];
+
+
+var joints = rawJoints.sort(
+  function (a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+    });
+
+//creates a new map marker
+function createMapMarker(latlng, address, name, url, id){
+ var marker = new google.maps.Marker({
+  position: latlng,
+  map: map,
+  id: id,
+  title: name
+ });
+if ($.inArray(marker, markerArray) === -1){
+marker.addListener('mouseover', function(){
+	$('#jointBox').scrollTo(document.getElementById("joint" + marker.id), 500);
+	$('#joint' + marker.id).animate({ backgroundColor: "blue"}, 250, function(){
+    	$('#joint' + marker.id).animate({ backgroundColor: "none"}, 500)
+    }
+     );
+    }
+);
+marker.addListener('click', function(){
+	$('#jointBox').scrollTo(document.getElementById("joint" + marker.id), 500);
+	$('#joint' + marker.id).animate({ backgroundColor: "blue"}, 250, function(){
+    	$('#joint' + marker.id).animate({ backgroundColor: "none"}, 500)
+    }
+     );
+    }
+);
+
+};
+markerArray.push(marker);
+};
+
+function findr(item){
+ return item.name == joint;
+};
